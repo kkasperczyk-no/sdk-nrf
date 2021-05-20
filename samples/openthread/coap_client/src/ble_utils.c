c38685b32941862bfb9397d375db7b8c4069a30c
Kamil Kasperczyk <kamil.kasperczyk@nordicsemi.no>
2021-05-20 15:10:53 +0200

    Added DFU over BLE using SMP support to the OT CoAP client sample
    This is a change allowing to test performing DFU over BLE
    with Thread enabled/disabled.
    
    Steps to test behavior:
    1. Build CoAP client sample with multiprotocol and dfu support:
    west build -b nrf52840dk_nrf52840 -- -DOVERLAY_CONFIG="overlay-multiprotocol_ble.conf;overlay-dfu_support.conf"
    2. Control Thread state in the application by invoking commands (by default it is enabled after boot):
    - ot state to check state
    - ot ifconfig down to disable Thread
    - ot ifconfig up + ot thread start to enable Thread again
    3. Navigate to the build/zephyr directory and trigger DFU using mcumgr command line tool:
    mcumgr --conntype ble --connstring ctlr_name=hci0,peer_name='NUS_CoAP_client' image upload app_update.bin
    4. Observe the behavior:
    - In case of success in logs DFU progress will be available. After finishing DFU it can be proceeded even further by calling:
    mcumgr --conntype ble --connstring ctlr_name=hci0,peer_name='NUS_CoAP_client' image list
    mcumgr --conntype ble --connstring ctlr_name=hci0,peer_name='NUS_CoAP_client' image test <image-sha>
    mcumgr --conntype ble --connstring ctlr_name=hci0,peer_name='NUS_CoAP_client' reset
    
    - In case of failure BLE connection will be broken.

--- samples/openthread/coap_client/src/ble_utils.c
+++ samples/openthread/coap_client/src/ble_utils.c
@@ -13,6 +13,20 @@
 
 #include "ble_utils.h"
 
+// MCUMgr BT FOTA includes
+#ifdef CONFIG_MCUMGR_CMD_OS_MGMT
+#include "os_mgmt/os_mgmt.h"
+#endif
+#ifdef CONFIG_MCUMGR_CMD_IMG_MGMT
+#include "img_mgmt/img_mgmt.h"
+#endif
+#ifdef CONFIG_MCUMGR_SMP_BT
+#include <mgmt/mcumgr/smp_bt.h>
+#endif
+#ifdef CONFIG_BOOTLOADER_MCUBOOT
+#include <dfu/mcuboot.h>
+#endif
+
 LOG_MODULE_REGISTER(ble_utils, CONFIG_BLE_UTILS_LOG_LEVEL);
 
 #define DEVICE_NAME CONFIG_BT_DEVICE_NAME
@@ -143,6 +157,14 @@ static void pairing_failed(struct bt_conn *conn, enum bt_security_err reason)
 	LOG_INF("Pairing failed conn: %s, reason %d", log_strdup(addr), reason);
 }
 
+int software_update_confirmation_handler(uint32_t offset, uint32_t size, void * arg)
+{
+    // For now just print update progress and confirm data chunk without any additional checks.
+    LOG_INF("Software update progress %d B / %d B", offset, size);
+
+    return 0;
+}
+
 int ble_utils_init(struct bt_nus_cb *nus_clbs,
 		   ble_connection_cb_t on_connect,
 		   ble_disconnection_cb_t on_disconnect)
@@ -182,6 +204,11 @@ int ble_utils_init(struct bt_nus_cb *nus_clbs,
 		LOG_ERR("Advertising failed to start (error: %d)", ret);
 		goto end;
 	}
+
+    os_mgmt_register_group();
+    img_mgmt_register_group();
+    img_mgmt_set_upload_cb(software_update_confirmation_handler, NULL);
+    smp_bt_register();
 
 end:
 	return ret;
